170fac835ac1d296b9270bfde6b9cb1557f2915d
Shaun <shawley64@gmail.com>
2015-04-30 16:53:40 -0600

    made changes to forms so they now are tied together.

--- src/FormFramework/elements/ComboBox.java
+++ src/FormFramework/elements/ComboBox.java
@@ -2,11 +2,12 @@ package FormFramework.elements;
 
 import FormFramework.validation.Validator;
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComboBox extends JComboBox implements FormElement{
 
-    private List<Validator> validators = null;
+    private List<Validator> validators = new ArrayList<Validator>();
 
     public ComboBox() {
         super();
