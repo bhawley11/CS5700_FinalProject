b76604d89fc98db3269d6534f8fb01494ed6a109
Shaun <shawley64@gmail.com>
2015-04-30 22:22:34 -0600

    Fixed succuss bug

--- src/FormFramework/action/DefaultActionHandler.java
+++ src/FormFramework/action/DefaultActionHandler.java
@@ -7,5 +7,6 @@ import javax.swing.*;
 public class DefaultActionHandler implements ActionHandler {
     public void execute(Form form) {
             form.setVisible(false);
+            form.setEnabled(false);
     }
 }
